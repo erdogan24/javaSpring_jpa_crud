6b5229dcb7564fd311c16906803fae29f6cf4c4a
erdoganakpolat <erdoganakpolat98@gmail.com>
2025-08-15 20:05:53 +0300

    commit 2

--- src/main/java/com/projectDemo/cruddemo/CruddemoApplication.java
+++ src/main/java/com/projectDemo/cruddemo/CruddemoApplication.java
@@ -1,5 +1,7 @@
 package com.projectDemo.cruddemo;
 
+import com.projectDemo.cruddemo.dao.StudentDAO;
+import com.projectDemo.cruddemo.entity.Student;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -13,10 +15,27 @@ public class CruddemoApplication {
 	}
 
 	@Bean
-	public CommandLineRunner commandLineRunner(String[] args){
+	public CommandLineRunner commandLineRunner(StudentDAO studentDAO){
 
 		return runner -> {
-			System.out.println("Hello World");
+			createStudent(studentDAO);
 		};
 	}
+
+	private void createStudent(StudentDAO studentDAO) {
+
+
+		// create the student object
+
+		System.out.println("Creating new student object..");
+		Student tempStudent = new Student("Erdogan", "Akpolat", "erdoganakplt.com");
+
+		// save the student object
+		System.out.println("Saving the student...");
+		studentDAO.save(tempStudent);
+
+		// display id of the saved student
+
+		System.out.println("Saved student. Generated id:" + tempStudent.getId());
+		}
 }
